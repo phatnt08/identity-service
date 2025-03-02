3dfbbb16350ad89a568f7d4c4f6808343d25970d
phatnt08 <phatnt08@gmail.com>
2025-03-02 20:38:04 +0700

    add endpoint get me

--- src/main/java/com/ntp/identity_service/configuration/SecurityConfig.java
+++ src/main/java/com/ntp/identity_service/configuration/SecurityConfig.java
@@ -19,8 +19,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.ntp.identity_service.enums.Role;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
